6747fb77059576bda658f4fbdb11fd6e59cb0eab
Yakov <jakovbraver@gmail.com>
2026-01-10 01:40:41 +0200

    Generalise 1D to n components

--- examples/example-1D.jl
+++ examples/example-1D.jl
@@ -13,21 +13,33 @@ end
 Float = Float32 # operating type
 
 xlimits = (-2π, 2π) .|> Float
-M = 50
+M = 100
 
-dh = DenseHamiltonian1D(xlimits; isperiodic=true, iseven=true, M, 𝑈)
+dh = XSpaceHamiltonian{:dense}(𝑈, xlimits; isperiodic=true, 𝑈_iseven=true, M)
 @time diagonalize!(dh, nev=5);
 dh.ε
 
-# For real Hamiltonian (ensured by `iseven=true`), the eigenstates can be chosen real. But since we are using a complex cis basis, the coefficients are complex.
+# For real Hamiltonian matrix (in momentum space), the eigenstates can be chosen real. But since we are using a complex cis basis, the coordinate-space functions are complex.
 # However, if potential is even, the eigenstates have definite parity. E.g. ground state is even, and so can be expressed in terms of cos, i.e. the real part of cis.
 # First excited state is odd, and so can be expressed in terms of sin, i.e. the imaginary part of cis.
 
-stateno = 5
+stateno = 4
 part = iseven(stateno-1) ? real : imag
-xs, ψ = make_eigenfunctions(dh; statenos=[stateno], nx=M)
+xs, ψ_sin = make_eigenfunctions(dh; statenos=[stateno], nx=200)
 plot(xs, 𝑈)
-plot!(xs, part(ψ[:, 1]) .+ dh.ε[stateno])
+plot!(xs, part(ψ[:, 1, 1]) .+ dh.ε[stateno])
+
+# Or we can solve using sine basis, yielding real coordinate-space eigenfunctions
+
+dh = XSpaceHamiltonian{:dense}(𝑈, xlimits; isperiodic=false, M)
+@time diagonalize!(dh, nev=5);
+dh.ε
+
+stateno = 4
+xs, ψ = make_eigenfunctions(dh; statenos=[stateno], nx=200)
+plot(xs, 𝑈)
+plot!(xs, ψ[:, 1, 1] .+ dh.ε[stateno])
+
 
 ### Lattice potential
 
@@ -41,9 +53,9 @@ U₀::Float = 50
 ϵ::Float = 0.15
 
 xlimits = (-2π, 2π) .|> Float
-M = 100
+M = 50
 
-dh = DenseHamiltonian1D(xlimits; isperiodic=true, iseven=true, M, 𝑈)
+dh = XSpaceHamiltonian{:dense}(𝑈, xlimits; isperiodic=true, 𝑈_iseven=true, M)
 @time diagonalize!(dh, nev=10);
 foreach(println, dh.ε)
 
@@ -51,7 +63,7 @@ foreach(println, dh.ε)
 statenos = 1:4
 xs, ψ = make_eigenfunctions(dh; statenos, nx=500)
 plot(xs, 𝑈, label=false)
-plot!(xs, abs2.(ψ) .+ dh.ε[statenos]', label=statenos) # in this system we can have (two-fold) degeneracies, so extracting real wf's is not so straightforward. Therefore, plotting densities
+plot!(xs, abs2.(ψ[:, 1, :]) .+ dh.ε[statenos]', label=statenos) # in this system we can have (two-fold) degeneracies, so extracting real wf's is not so straightforward. Therefore, plotting densities
 
 # study wanniers
 @time compute_wanniers!(dh, targetlevels=1:4);
@@ -64,12 +76,11 @@ plot!(xs, w_real .+ dh.wanniers.E')
 
 plot(xs, imag.(w[:, 1]))
 
-
 # study one cell using quasimomentum
 
 xlimits = (-π/2, π/2) .|> Float
-dh = DenseHamiltonian1D(xlimits; isperiodic=true, iseven=false, M, 𝑈)
-ncells = 5
+dh = XSpaceHamiltonian{:dense}(𝑈, xlimits; isperiodic=true, 𝑈_iseven=true, M)
+ncells = 4
 qs = range(0, 1, ncells)
-@benchmark diagonalize!(dh, qs; nev=0)
+diagonalize!(dh, qs; nev=10)
 dh.ε_q
