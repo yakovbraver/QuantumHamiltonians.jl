5142469dbb48597e2a1a599312ae7fa52735a25b
Yakov <jakovbraver@gmail.com>
2026-01-24 22:32:10 +0200

    Refine argument type annotations

--- src/momentum.jl
+++ src/momentum.jl
@@ -2,9 +2,9 @@
 Return the momentum-squared matrix (𝛿𝑝)² = -𝛿²Δ in `D = length(L)` dimensions. If `basis=:cis`, then return (𝛿𝑝 + 𝑞)² with quasimomenta provided in `qs`.
 A real `Diagonal` matrix is returned.
 """
-function make_p²(L::AbstractVector{R}, M::Integer, δ::Real, basis::Symbol, qs=zeros(R, length(L))) where R <: AbstractFloat # ntuple(Returns(zero(R)), length(L))
+function make_p²(L::AbstractVector{R}, M::Integer, δ::R, basis::Symbol, qs=zeros(R, length(L))) where R <: AbstractFloat # ntuple(Returns(zero(R)), length(L))
     D = length(L)
-    PI = R(π)
+    PI = R(π) # for type stability. Otherwise 2*π*2f0 gets converted to Float64 (and so does 2f0*2π, while 2f0*2*π does not...)
     if basis == :cis
         cx = 2PI*δ/L[1]
         if D == 1
@@ -21,11 +21,14 @@ function make_p²(L::AbstractVector{R}, M::Integer, δ::Real, basis::Symbol, qs=
             by = PI*δ/L[2]
             return [(bx*jx)^2 + (by*jy)^2 for jx in 1:M for jy in 1:M] |> Diagonal
         end
+    else
+        error("make_p² not implemented for basis $basis.")
     end
+    return Diagonal(R[]) # for type stability
 end
 
 "Return the matrix of 𝑝ᵢ = -iδ𝜕ᵢ if `basis=:cis` and δ𝜕ᵢ otherwise. The output is real in both cases."
-function make_p_i(L::AbstractVector{<:AbstractFloat}, M::Integer, δ::Real, basis::Symbol, i::Int)
+function make_p_i(L::AbstractVector{R}, M::Integer, δ::R, basis::Symbol, i::Integer) where R <: AbstractFloat
     if i == 1
         return make_p_x(L, M, δ, basis)
     elseif i == 2
@@ -34,18 +37,18 @@ function make_p_i(L::AbstractVector{<:AbstractFloat}, M::Integer, δ::Real, basi
 end
 
 "Return the matrix of 𝑝ₓ = -iδ𝜕ₓ if `basis=:cis` and δ𝜕ₓ otherwise. The output is real in both cases."
-function make_p_x(L::AbstractVector{<:AbstractFloat}, M::Integer, δ::Real, basis::Symbol)
+function make_p_x(L::AbstractVector{R}, M::Integer, δ::R, basis::Symbol) where R <: AbstractFloat
     D = length(L)
     Lx = L[1]
     if basis == :cis
-        PI = typeof(Lx)(π)
+        PI = R(π)
         if D == 1
             return Diagonal([2PI * δ * jx/Lx for jx in -M:M])
         elseif D == 2
             return Diagonal([2PI * δ * jx/Lx for jx in -M:M for jy in -M:M])
         end
     elseif basis == :sin
-        ∂_x = zeros(typeof(Lx), M^D, M^D)
+        ∂_x = zeros(R, M^D, M^D)
         if D == 1
             ##
         elseif D == 2
@@ -60,15 +63,15 @@ function make_p_x(L::AbstractVector{<:AbstractFloat}, M::Integer, δ::Real, basi
 end
 
 "Return the matrix of 𝑝_𝑦 = -i𝛿𝜕_𝑦 if `basis=:cis` and 𝛿𝜕_𝑦 otherwise. The output is real in both cases."
-function make_p_y(L::AbstractVector{<:AbstractFloat}, M::Integer, δ::Real, basis::Symbol)
+function make_p_y(L::AbstractVector{R}, M::Integer, δ::R, basis::Symbol) where R <: AbstractFloat
     D = length(L)
     # D == 1 && @error "Only D ≥ 2 supported. For D = 1, use make_p_x."; return
     Ly = L[2]
-    PI = typeof(Ly)(π)
     if basis == :cis
+        PI = R(π)
         return Diagonal([2PI * δ * jy/Ly for jx in -M:M for jy in -M:M])
     elseif basis == :sin
-        ∂_y = zeros(typeof(Ly), M^D, M^D)
+        ∂_y = zeros(R, M^D, M^D)
         @floop for jx in 1:M
             for jy in 1:M, j′y in 1+isodd(jy):2:M
                 ∂_y[(jx-1)M+j′y, (jx-1)M+jy] = 4δ * j′y*jy/(Ly * (j′y^2 - jy^2))
